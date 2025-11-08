4063bf1ab68957dec2d79e8213a5a73247fcc3b9
nohl4n <nohlanvaureal@gmail.com>
2025-11-08 02:17:01 +0100

    V1.0

--- python/SL.py
+++ python/SL.py
@@ -125,6 +125,39 @@ def Contour(M,i,j):
         
     return C
     
+def Contour_ind(M,i,j):
+
+    n,m = len(M) , len(M[0])
+    
+    if i==0 and j==0:
+        C =[[i,j+1],[i+1,j+1],[i+1,j]]
+                    
+    elif i==n-1 and j==0:
+        C =[[i-1,j],[i-1,j+1],[i,j+1],False]
+        
+    elif i==0 and j==m-1:
+        C =[[i,j-1],[i+1,j-1],[i+1,j]]
+                              
+    elif i==n-1 and j==m-1:
+        C =[[i-1,j-1],[i-1,j],[i,j-1]]
+                    
+    elif i==0:
+        C =[[i,j-1],[i+1,j-1],[i+1,j],[i+1,j+1],[i,j+1]]
+                    
+    elif j==0:
+        C =[[i-1,j],[i-1,j+1],[i,j+1],[i+1,j+1],[i+1,j],False]
+                    
+    elif i==n-1:
+        C =[[i-1,j-1],[i-1,j],[i-1,j+1],[i,j+1],[i,j-1]]
+                    
+    elif j==m-1:
+        C =[[i-1,j-1],[i-1,j],[i+1,j],[i+1,j-1],[i,j-1]]
+    
+    else:
+        C =[[i-1,j-1],[i-1,j],[i-1,j+1],[i,j+1],[i+1,j+1], [i+1,j], [i+1,j-1],[i,j-1]]
+        
+    return C
+    
 def modifiable(M,i,j):
     """Verifie si une case peut être à la fois exterieur et interieur"""
     
@@ -160,7 +193,7 @@ def grignotage_rec(n, m):
     np.array: Matrice booléenne représentant le puzzle
     """
 
-    L =[[True]*m]*n
+    L =[[True]*m for _ in range(n)]
     M = np.array(L)
     B= bordure(0, n, 0, m)
     b=len(B)
@@ -169,7 +202,7 @@ def grignotage_rec(n, m):
     
     for k in range(a):
         if modifiable(M, B[k][0], B[k][1]):
-            generate_rec(M, B[k][0], B[k][1], n*m//2)
+            generate_rec(M, B[k][0], B[k][1], n*m)
     
     return M
     
@@ -186,7 +219,7 @@ def grignotage_rec_centre(n, m):
         np.array: Matrice booléenne représentant le puzzle
     """
 
-    L =[[True]*m]*n
+    L =[[True]*m for _ in range(n)]
     M = np.array(L)
     generate_rec(M, n//2, m//2, (n*m)//2)
     
@@ -202,6 +235,7 @@ def inverse(M):
     Returns:
         np.array: Matrice inversée
     """
+    
     n, m = len(M) ,  len(M[0])
     for i in range(n):
         for j in range(m):
@@ -310,7 +344,7 @@ def bruit (n, m, r):
         tuple: (Matrice bruitée,  Historique des positions modifiées)
     """
     
-    L = [[False]*m]*n
+    L = [[False]*m for _ in range(n)]
     M = np.array(L)
     H = []
     
@@ -460,7 +494,7 @@ def generate_chemins(n, m):
         np.array: Matrice du puzzle
     """
     
-    L =[[True]*m]*n
+    L =[[True]*m for _ in range(n)]
     M = np.array(L)
     B= bordure(0, n, 0, m)
     
@@ -479,7 +513,7 @@ def generate_chemins(n, m):
     return M
     
 def generate_chemins_complexes(n, m):
-    L =[[True]*m]*n
+    L =[[True]*m for _ in range(n)]
     M = np.array(L)
     B= bordure(0, n, 0, m)
     
@@ -537,7 +571,7 @@ def grignotage_carré(M, n, m):
         np.array: Matrice du puzzle
     """
 
-#    L =[[True]*m]*n
+#    L =[[True]*m for _ in range(n)]
 #    M = np.array(L)
     B= bordure(0, n, 0, m)
     b=len(B)
@@ -831,6 +865,26 @@ def List_modifiable(M):
                 L.append([i,j])
     
     return L
+    
+def List_bout(M):
+    """renvoie une liste des indices des cases qui sont des bout"""
+    n,m = M.shape
+    L=[]
+    
+    for i in range(n):
+        for j in range(m):
+                
+            acc = 0
+            C = Croix(M,i,j)
+            
+            for b in C:
+                if b:
+                    acc += 1
+                    
+            if  (M[i][j] and acc ==3) or ( ( not M[i][j] ) and acc == 1 ) :
+                L.append([i,j])
+    
+    return L
 
 """
 ~~~~~~~~~~~~~~~~~~~
@@ -867,6 +921,9 @@ def List_nxm(n,m):
     f += 'b'
     
     for k in range(2**(n*m)):
+    
+        if k % 10000 == 0:
+            print(k," ieme matrice crée")
         s = list(format(k,f))
     
         for p in range (n*m):
@@ -875,7 +932,7 @@ def List_nxm(n,m):
             else:
                 s[p] = True
                 
-        M = [[False]*m]*n
+        M = [[False]*m for _ in range(n)]
         M=np.array(M)
         somme = 0
     
@@ -943,9 +1000,13 @@ def filtre_nombre_modifiable(L,mini,maxi):
             
     return L_filtre
     
+
+
 #___________Test_de_bon_retour_pour_les_critères
 
 def in_pas_diag(M):
+    """Verifie si une matrice de True est false ne contient pas des True
+    ou False en diagonale (ce qui rend impossible le fait d'être un puzzle SL"""
     
     res =True
     n,m = len(M),len(M[0])
@@ -1191,7 +1252,7 @@ def Rule1_ind(M,i,j,NA):
             if NA[i][j]!=p:
                 res = False
                 
-def NA(M):
+def Nombre_Arrete(M):
 
     """
     Calcule la matrice des nombres d'arrête.
@@ -1392,7 +1453,7 @@ def show_n(L,n,m):
     plt.show()
     
 def show_anim(L, interval=200):
-
+    n,m = len(M) , len(M[0])
     """
     Affiche une animation d'une séquence de matrices.
     
@@ -1417,3 +1478,392 @@ def show_anim(L, interval=200):
     
     plt.show()
     return anim
+    
+"""
+~~~~~~~~~~~~~~~~~~~
+V - Solve puzzle
+~~~~~~~~~~~~~~~~~~~
+"""
+
+def List_puzzle(n,m):
+
+    L = List_nxm(n,m)
+    print("L contient ",len(L),"élément")
+    L1 = filtre_diag(L)
+    print("filtre_diag : OK ",len(L1))
+    #L2 = filtre_equiv(L1)
+    #print("filtre_equiv : OK ",len(L2))
+    L3 = filtre_puzzle(L1)
+    print("filtre_puzzle : OK ",len(L3))
+    
+    return L3
+
+def Brut_force(Na):
+    """test tout"""
+    Na = Na.tolist()
+    n,m = len(Na),len(Na[0])
+    
+    L = List_puzzle(n,m)
+    l = len (L)
+    
+    for i in range(l):
+        M = Nombre_Arrete(L[i]).tolist()
+        
+        if i%10000 == 0:
+            print(i)
+
+        if M == Na :
+            print("trouvé !")
+            return L[i]
+            
+def Test(Na,L):
+    l = len (L)
+    Na= Na.tolist()
+    
+    for i in range(l):
+        Na_t = Nombre_Arrete(L[i]).tolist()
+        
+        if i%10000 == 0:
+            print(i)
+
+        if Na_t == Na :
+            print("trouvé !")
+            return L[i] 
+            
+
+"""-----------------------------------------"""
+
+def Solve_par_essaie_modifie(N,tentative = 100):
+
+    n,m = len(N) , len(N[0])
+    
+    #genere une matrice puzzle
+    
+    M = grignotage_rec(n,m)
+    L = []
+    
+    
+    for _ in range(tentative):
+        
+        
+        #pour voir la progression
+        M_p = c.deepcopy(M)
+        L.append(M_p)
+        
+        #regarde les modifiables et le score de la matrice
+        L_modif = List_modifiable(M)
+        l_modif = len(L_modif)
+        score = Score(M,N,m,n)
+        print(score,M)
+        
+        #On modifie aleatoirement les modifiables jusqu'a ce que le score soit <
+        nb_essaie = 2**(l_modif)
+        while Score(M,N,m,n) <= score and nb_essaie>0:
+            nb_essaie -= 1
+            L_random = List_random(l_modif)
+            
+            for k in range(l_modif):
+            
+                i,j = L_modif[k]
+            
+                if L_random == 1:
+                    M[i,j] = True
+                    
+                else:
+                    M[i,j] = False      
+                
+        #si c'est la matrice c'est bon
+        if nb_essaie <=0:
+            print("pas moyen de monter le score")
+            M = grignotage_rec(n,m)
+        
+        if Score(M,N,m,n) == n*m:
+            print("trouvé")
+            return M
+            
+    print("pas trouvé")
+   
+    return L
+                        
+def NA_ind(M,i,j):
+    C = Croix(M,i,j)
+    tc=len(C)
+    nb=0
+    if M[i][j]:
+        nb= 4-tc
+        for k in range(tc):
+            if not C[k]:
+                nb+=1
+        
+    elif not M[i][j]:
+        for k in range(tc):
+            if C[k]:
+                nb+=1
+                
+    return nb
+    
+def List_random(n):
+    L=[]
+    for i in range (n):
+        ra = r.randint(0,1)
+        
+        if ra == 0:
+            L.append(False)
+        else:
+            L.append(True)
+            
+    return L
+    
+def Score(M,Na,n,m):
+
+    Na_m = Nombre_Arrete(M)
+    s = 0
+    
+    for i in range(n):
+        for j in range(m):
+            if Na_m[i,j] == Na[i,j]:
+                s+=1
+                
+    return s
+    
+"""~~~~~idée~Backtracking~~~~~~~~~~~~~~~~~~~~~"""
+        
+class SolutionFound(Exception):
+    pass
+
+def solve3(S):
+    n, m = len(S), len(S[0])
+    M = [[0]*m for _ in range(n)]
+    try:
+        # on tente d'abord en posant la première case à 1 puis 2
+        rec_brut3(M, S, 0, 0, 1)
+        rec_brut3(M, S, 0, 0, 2)
+        print("Aucune solution trouvée.")
+    except SolutionFound:
+        print(" Solution trouvée !")
+
+def rec_brut3(M, S, i, j, Val):
+    # sauvegarde pour le backtracking
+    prev = M[i][j]
+    M[i][j] = Val
+
+    # affichage pour debug (on affiche une copie pour éviter effet de référence)
+    for row in M:
+        print(row)
+    print("--")
+
+    # test de solution
+    if M == S:
+        raise SolutionFound
+
+    n = len(M)
+    m = len(M[0])
+
+    # avancer dans l'ordre des cases : (i,j) -> next_i, next_j
+    if j < m - 1:
+        # on explore la case suivante sur la même ligne (i, j+1)
+        rec_brut3(M, S, i, j+1, 1)
+        rec_brut3(M, S, i, j+1, 2)
+    elif i < n - 1:
+        # fin de colonne : on passe à la ligne suivante, première colonne
+        rec_brut3(M, S, i+1, 0, 1)
+        rec_brut3(M, S, i+1, 0, 2)
+    # sinon nous sommes à la dernière case et on a déjà testé la solution
+
+    # backtrack : restaurer l'ancienne valeur avant de retourner
+    M[i][j] = prev
+    
+
+def solve_SL(Na):
+    n, m = len(Na), len(Na[0])
+    M = [[False]*m for _ in range(n)]
+    Na_M = [[0]*m for _ in range(n)]
+    try:
+        # on tente d'abord en posant la première case à 1 puis 2
+        if Na[0][0] >= 2:
+            Na_M,restore = modifie_Na_M(M,Na_M,0,0)
+            
+            rec_brut_SL(M, Na_M, Na, 0, 0, True)
+            
+            Na_M = restore_Na_M(Na_M,restore)
+        if Na[0][0] <= 2:
+            rec_brut_SL(M, Na_M, Na, 0, 0, False)
+        print("Aucune solution trouvée.")
+    except SolutionFound:
+        print(" Solution trouvée !")
+
+def rec_brut_SL(M, Na_M, Na, i, j, Val):
+    # sauvegarde pour le backtracking
+    prev = M[i][j]
+    M[i][j] = Val
+    
+    n = len(M)
+    m = len(M[0])
+
+    # affichage pour debug (on affiche une copie pour éviter effet de référence)
+    
+    for row in range(n):
+        print(M[row],Na_M[row],Na[row],i,j)
+    print("--")
+
+    # test de solution
+    if Na_M == Na:
+        raise SolutionFound
+
+    # avancer dans l'ordre des cases : (i,j) -> next_i, next_j
+    # calcule next indices
+    next_i, next_j = i, j
+    if j < m - 1:
+        next_i, next_j = i, j + 1
+    elif i < n - 1:
+        next_i, next_j = i + 1, 0
+    else:
+        # dernière case ; on a déjà testé l'égalité, donc pas de suite
+        # restore et return
+        M[i][j] = prev
+        return
+
+    # obtenir états possibles pour la prochaine case
+    E = états_possible(M, Na_M, Na, next_i, next_j)
+
+    # explore selon ce qui est possible (tester présence plutôt qu'égalité stricte)
+    if True in E:
+        #modifie Na
+        Na_M,restore = modifie_Na_M(M,Na_M,next_i,next_j)
+        
+        rec_brut_SL(M, Na_M, Na, next_i, next_j, True)
+        
+        # restore l'ancienne valeur de Na_M
+        Na_M = restore_Na_M(Na_M,restore)
+    if False in E:
+        rec_brut_SL(M, Na_M, Na, next_i, next_j, False)
+
+    # backtrack : restaurer l'ancienne valeur avant de retourner
+    M[i][j] = prev
+
+
+def modifie_Na_M(M,Na_M,i,j):
+    restore = []
+    C = Croix_ind(M,i,j)
+    
+    val = 0
+    c = len(C)
+    
+    for ic,jc in C:
+        if M[ic][jc] == True :
+            restore.append([ic,jc, Na_M[ic][jc]])
+            Na_M[ic][jc] = Na_M[ic][jc] - 1
+        else :
+            restore.append([ic,jc, Na_M[ic][jc]])
+            Na_M[ic][jc] = Na_M[ic][jc] + 1
+            val += 1
+    
+    restore.append([i,j,Na_M[i][j]])
+    Na_M[i][j] = val + (4 -c)
+    return Na_M,restore
+    
+def restore_Na_M(Na_M,restore):
+    for i,j,val in restore:
+        Na_M[i][j] = val
+        
+    return Na_M
+            
+            
+
+    
+def états_possible(M,Na_M,Na,i,j):
+    E = []
+    if i == 0 :
+        if j == 0 or condition_droite(M,Na_M,Na,i,j) == "pas de condition":
+            if condition_mettre_in(M,Na_M,Na,i,j):
+                E.append(True)
+            if condition_mettre_out(M,Na_M,Na,i,j):
+                E.append(False)
+        elif condition_droite(M,Na_M,Na,i,j) == True:
+            if condition_mettre_in(M,Na_M,Na,i,j):
+                E.append(True)
+        else : 
+            if condition_mettre_out(M,Na_M,Na,i,j):
+                E.append(False)
+                
+    else :
+        if condition_bas(M,Na_M,Na,i,j) == True :
+            if j == 0 or condition_droite(M,Na_M,Na,i,j) == "pas de condition":
+                if condition_mettre_in(M,Na_M,Na,i,j):
+                    E.append(True)
+            elif condition_droite(M,Na_M,Na,i,j) == True:
+                if condition_mettre_in(M,Na_M,Na,i,j):
+                    E.append(True)
+        
+        else : 
+            if j == 0 or condition_droite(M,Na_M,Na,i,j) == "pas de condition":
+                if condition_mettre_out(M,Na_M,Na,i,j):
+                    E.append(False)
+            elif condition_droite(M,Na_M,Na,i,j) == False:
+                if condition_mettre_out(M,Na_M,Na,i,j):
+                    E.append(False)
+                    
+    return E
+        
+        
+def condition_bas(M,Na_M,Na,i,j):
+    # l'état en dessous ne pouvant plus être modifier après
+    if Na_M[i-1][j] == Na[i-1][j]:
+        return False
+    else: # il faut le modifier
+        return True
+
+def condition_droite(M,Na_M,Na,i,j):
+    # état de droit ne pourras être modifier plus que 1 fois après
+    if M[i][j-1] and Na_M[i][j-1] + 2 == Na[i][j-1] : 
+        # si l'etat est in et doit être modifier 2 fois
+        return True
+    elif (not M[i][j-1]) and Na_M[i][j-1] - 2 == Na[i][j-1] :
+        # si l'etat est out et doit être modifier 2 fois
+        return True
+    elif Na_M[i][j-1] == Na[i][j-1] :
+        # si l'etat final déjà atteint il ne faut plus le modifier
+        return False
+    else : # les deux états sont possibles
+        return "pas de condition"
+
+def condition_mettre_out(M,Na_M,Na,i,j):
+    # vérifier qu'il n'y a pas de contradiction si on laisse out
+    n = len(M)
+    m = len(M[0])
+
+    val_si_out = Na_M[i][j]
+    
+    if i+1 == n and j+1 == m :
+        # On ne plus apporté de modification
+        return val_si_out == Na[i][j]
+        
+    elif j+1 == m :
+        return Na[i][j] - val_si_out >= 0 and Na[i][j] - val_si_out <= 1
+    
+    else :
+        return Na[i][j] - val_si_out >= 0 and Na[i][j] - val_si_out <= 2
+
+def condition_mettre_in(M,Na_M,Na,i,j):
+    # vérifier qu'il n'y a pas de contradiction si on met in
+    n = len(M)
+    m = len(M[0])
+
+    C = Croix(M,i,j)
+    nb= 0
+    for k in range(4):
+        if not C[k]:
+            nb+=1
+    
+    val_si_in = nb
+    
+    if i+1 == n and j+1 == m :
+        # On ne plus apporté de modification
+        return val_si_in == Na[i][j]
+        
+    elif j+1 == m :
+        return val_si_in - Na[i][j] >= 0 and val_si_in - Na[i][j] <= 1
+    
+    else :
+        return val_si_in - Na[i][j] >= 0 and val_si_in - Na[i][j] <= 2
+
---
+++ python/Solver_SL1.py
@@ -0,0 +1,229 @@
+        
+class SolutionFound(Exception):
+    pass
+
+def solve3(S):
+    n, m = len(S), len(S[0])
+    M = [[0]*m for _ in range(n)]
+    try:
+        # on tente d'abord en posant la première case à 1 puis 2
+        rec_brut3(M, S, 0, 0, 1)
+        rec_brut3(M, S, 0, 0, 2)
+        print("Aucune solution trouvée.")
+    except SolutionFound:
+        print(" Solution trouvée !")
+
+def rec_brut3(M, S, i, j, Val):
+    # sauvegarde pour le backtracking
+    prev = M[i][j]
+    M[i][j] = Val
+
+    # affichage pour debug (on affiche une copie pour éviter effet de référence)
+    for row in M:
+        print(row)
+    print("--")
+
+    # test de solution
+    if M == S:
+        raise SolutionFound
+
+    n = len(M)
+    m = len(M[0])
+
+    # avancer dans l'ordre des cases : (i,j) -> next_i, next_j
+    if j < m - 1:
+        # on explore la case suivante sur la même ligne (i, j+1)
+        rec_brut3(M, S, i, j+1, 1)
+        rec_brut3(M, S, i, j+1, 2)
+    elif i < n - 1:
+        # fin de colonne : on passe à la ligne suivante, première colonne
+        rec_brut3(M, S, i+1, 0, 1)
+        rec_brut3(M, S, i+1, 0, 2)
+    # sinon nous sommes à la dernière case et on a déjà testé la solution
+
+    # backtrack : restaurer l'ancienne valeur avant de retourner
+    M[i][j] = prev
+    
+
+def solve_SL(Na):
+    n, m = len(Na), len(Na[0])
+    M = [[False]*m for _ in range(n)]
+    Na_M = [[0]*m for _ in range(n)]
+    try:
+        # on tente d'abord en posant la première case à 1 puis 2
+        if Na[0][0] >= 2:
+            rec_brut_SL(M, modifie_Na_M(M,Na_M,0,0), Na, 0, 0, True)
+        if Na[0][0] <= 2:
+            rec_brut_SL(M, Na_M, Na, 0, 0, False)
+        print("Aucune solution trouvée.")
+    except SolutionFound:
+        print(" Solution trouvée !")
+
+def rec_brut_SL(M, Na_M, Na, i, j, Val):
+    # sauvegarde pour le backtracking
+    prev = M[i][j]
+    M[i][j] = Val
+    
+    n = len(M)
+    m = len(M[0])
+
+    # affichage pour debug (on affiche une copie pour éviter effet de référence)
+    
+    for row in range(n):
+        print(M[row],Na_M[row],Na[row],i,j)
+    print("--")
+
+    # test de solution
+    if Na_M == Na:
+        raise SolutionFound
+
+    # avancer dans l'ordre des cases : (i,j) -> next_i, next_j
+    if j < m - 1:
+        # on explore la case suivante sur la même ligne (i, j+1)
+        E = états_possible(M,Na_M,Na,i,j+1)
+        
+        if E == [True,False] :
+            rec_brut_SL(M,modifie_Na_M(M,Na_M,i,j+1), Na, i, j+1, True)
+            rec_brut_SL(M,Na_M, Na, i, j+1, False)
+            
+        elif E == [True]:
+            rec_brut_SL(M,modifie_Na_M(M,Na_M,i,j+1), Na, i, j+1, True)
+            
+        elif E == [False]:
+            rec_brut_SL(M, Na_M, Na, i, j+1, False)
+        
+
+    elif i < n - 1:
+        # fin de colonne : on passe à la ligne suivante, première colonne
+        E = états_possible(M,Na_M,Na,i+1,0)
+        
+        if E == [True,False] :
+            rec_brut_SL(M,modifie_Na_M(M,Na_M,i+1,0), Na, i+1, 0, True)
+            rec_brut_SL(M,Na_M, i+1, 0, False)
+            
+        elif E == [True]:
+            rec_brut_SL(M,modifie_Na_M(M,Na_M,i+1,0), Na, i+1, 0, True)
+            
+        elif E == [False]:
+            rec_brut_SL(M, Na_M, Na, i+1, 0, False)
+    # sinon nous sommes à la dernière case et on a déjà testé la solution
+
+    # backtrack : restaurer l'ancienne valeur avant de retourner
+    M[i][j] = prev
+
+
+def modifie_Na_M(M,Na_M,i,j):
+    C = Croix_ind(M,i,j)
+    
+    val = 0
+    c = len(C)
+    
+    for ic,jc in C:
+        if M[ic][jc] == True :
+            Na_M[ic][jc] = Na_M[ic][jc] - 1
+        else :
+            Na_M[ic][jc] = Na_M[ic][jc] + 1
+            val += 1
+    
+    Na_M[i][j] = val + (4 -c)
+    return Na_M
+            
+            
+
+    
+def états_possible(M,Na_M,Na,i,j):
+    E = []
+    if i == 0 :
+        #if j == 0 or condition_droite(M,Na_M,Na,i,j) == "pas de condition":
+            #if condition_mettre_in(M,Na_M,Na,i,j):
+        E.append(True)
+            #if condition_mettre_out(M,Na_M,Na,i,j):
+        E.append(False)
+        #elif condition_droite(M,Na_M,Na,i,j) == True:
+            #if condition_mettre_in(M,Na_M,Na,i,j):
+            #E.append(True)
+        #else : 
+            #if condition_mettre_out(M,Na_M,Na,i,j):
+            #E.append(False)
+                
+    else :
+        if condition_bas(M,Na_M,Na,i,j) == True :
+            #if j == 0 or condition_droite(M,Na_M,Na,i,j) == "pas de condition":
+                #if condition_mettre_in(M,Na_M,Na,i,j):
+            E.append(True)
+            #elif condition_droite(M,Na_M,Na,i,j) == True:
+                #if condition_mettre_in(M,Na_M,Na,i,j):
+                #E.append(True)
+        
+        else : 
+            #if j == 0 or condition_droite(M,Na_M,Na,i,j) == "pas de condition":
+                #if condition_mettre_out(M,Na_M,Na,i,j):
+            E.append(False)
+            #elif condition_droite(M,Na_M,Na,i,j) == False:
+                #if condition_mettre_out(M,Na_M,Na,i,j):
+                #E.append(False)
+                    
+    return E
+        
+        
+def condition_bas(M,Na_M,Na,i,j):
+    # l'état en dessous ne pouvant plus être modifier après
+    if Na_M[i-1][j] == Na[i-1][j]:
+        return False
+    else: # il faut le modifier
+        return True
+
+def condition_droite(M,Na_M,Na,i,j):
+    # état de droit ne pourras être modifier plus que 1 fois après
+    if M[i][j-1] and Na_M[i][j-1] + 2 == Na[i][j-1] : 
+        # si l'etat est in et doit être modifier 2 fois
+        return True
+    elif (not M[i][j-1]) and Na_M[i][j-1] - 2 == Na[i][j-1] :
+        # si l'etat est out et doit être modifier 2 fois
+        return True
+    elif Na_M[i][j-1] == Na[i][j-1] :
+        # si l'etat final déjà atteint il ne faut plus le modifier
+        return False
+    else : # les deux états sont possibles
+        return "pas de condition"
+
+def condition_mettre_out(M,Na_M,Na,i,j):
+    # vérifier qu'il n'y a pas de contradiction si on laisse out
+    n = len(M)
+    m = len(M[0])
+
+    val_si_out = Na_M[i][j]
+    
+    if i+1 == n and j+1 == m :
+        # On ne plus apporté de modification
+        return val_si_out == Na[i][j]
+        
+    elif j+1 == m :
+        return Na[i][j] - val_si_out >= 0 and Na[i][j] - val_si_out <= 1
+    
+    else :
+        return Na[i][j] - val_si_out >= 0 and Na[i][j] - val_si_out <= 2
+
+def condition_mettre_in(M,Na_M,Na,i,j):
+    # vérifier qu'il n'y a pas de contradiction si on met in
+    n = len(M)
+    m = len(M[0])
+
+    C = Croix(M,i,j)
+    nb= 0
+    for k in range(4):
+        if not C[k]:
+            nb+=1
+    
+    val_si_in = nb
+    
+    if i+1 == n and j+1 == m :
+        # On ne plus apporté de modification
+        return val_si_in == Na[i][j]
+        
+    elif j+1 == m :
+        return val_si_in - Na[i][j] >= 0 and val_si_in - Na[i][j] <= 1
+    
+    else :
+        return val_si_in - Na[i][j] >= 0 and val_si_in - Na[i][j] <= 2
+
